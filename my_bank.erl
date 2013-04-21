9d3f6dc1058fa5b87dc7229eaec0a79675328652
yangxuesong <yxs815@gmail.com>
2013-04-21 22:27:47 +0800

    bank

--- my_bank.erl
+++ my_bank.erl
@@ -1,4 +1,4 @@
--module(my_bank).
+-mo	dule(my_bank).
 -behaviour(gen_server).
 -export([start/0, stop/0, new_account/1, withdraw/2, deposit/2, query/1, init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3, format_status/2]).
 
@@ -14,7 +14,36 @@ query(Who) -> gen_server:call(?MODULE, {query, Who}).
 %gen_server callbacks which executes in ?MODULE process
 init(Args) -> {ok, ets:new(?MODULE, [])}.
 
-handle_call(Request, From, State) -> Reply = ok, NewState = state, {reply, Reply, NewState}.
+handle_call({new, Who}, From , Tab) ->
+	Reply = case ets:lookup(Tab, Who) of
+		[] -> ets:insert(Tab, {Who, 0}),
+			{welcome, Who};
+		[_] -> {Who, you_already_are_a_custom}
+	 {reply, Reply, Tab};
+handle_call({query, Who}, From, Tab) ->
+	Reply = case ets:lookup(Tab, Who) of
+		[] -> {Who, you_are_not_a_custom_yet};
+		[{Who, Number}] -> {Who, Number}
+	{reply, Reply, Tab};
+handle_call({withdraw, Who, Number}, From, Tab) ->
+	Reply = case ets:lookup(Tab, Who) of
+		[] -> {Who, you_are_not_a_custom_yet};
+		[{Who, Amount}] when Amount >= Number ->
+			NewAmount = Amount - Number,
+			ets:insert(Tab, {Who, NewAmount},
+			{Who, NewAmount};
+		[{Who, Amount}] -> {Who, not_enougth_money}
+	{reply, Reply, Tab};
+handle_call({deposit, Who, Number}, From, Tab) ->
+	Reply = case ets:lookup(Tab, Who) of
+		[] -> {Who, you_are_not_a_custom_yet};
+		[{Who, Amount}] -> 
+			NewAmount = Amount + Number,
+			ets:lookup(Tab, {Who, NewAmount},
+			{Who, NewAmount}
+	{reply, Reply, Tab};
+handle_call(stop, From, Tab) -> 
+	{stop, normal, stopped, Tab}.
 
 handle_cast(Request, State) -> {noreply, State}.
 handle_info(Info, State) -> {noreply, State}.
